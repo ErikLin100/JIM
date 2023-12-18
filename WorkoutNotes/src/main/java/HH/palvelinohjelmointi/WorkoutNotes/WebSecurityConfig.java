9b43729c784a723ac95821c999ea4c604137f86f
ErikLin100 <bgr@myy.haaga-helia.fi>
2023-12-18 18:50:06 +0200

    old project found

--- WorkoutNotes/src/main/java/HH/palvelinohjelmointi/WorkoutNotes/WebSecurityConfig.java
+++ WorkoutNotes/src/main/java/HH/palvelinohjelmointi/WorkoutNotes/WebSecurityConfig.java
@@ -1,9 +1,8 @@
 package HH.palvelinohjelmointi.WorkoutNotes;
 
-import java.util.Locale;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.web.WebProperties.LocaleResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -12,7 +11,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 
 
